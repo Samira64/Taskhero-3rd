2aa8689b663d4873d9dcdbac7e53978b4a9122cf
samira <s.soltani336@gmail.com>
2016-06-03 00:07:27 +0800

    HTML and CSS codes indented properly and Javascript task 1 done

---
+++ js/main.js
@@ -0,0 +1,14 @@
+
+var taskInput= document.getElementById("new-task-input");
+
+
+taskInput.addEventListener("keydown", checkKeyPressed, true);
+ 
+function checkKeyPressed(e) {
+	
+
+    if (e.keyCode == "13") {
+        var taskValue = taskInput.value;
+        alert("Adding the task" + " " + taskValue);
+    }
+};
