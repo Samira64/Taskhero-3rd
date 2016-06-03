82df6e593d18cdb225584cbedd18b06036beb94e
samira <s.soltani336@gmail.com>
2016-06-03 13:53:06 +0800

    empty files removed from the JS file

--- js/main.js
+++ js/main.js
@@ -1,12 +1,7 @@
 
 var taskInput= document.getElementById("new-task-input");
-
-
 taskInput.addEventListener("keydown", checkKeyPressed, true);
- 
-function checkKeyPressed(e) {
-	
-
+ function checkKeyPressed(e) {
     if (e.keyCode == "13") {
         var taskValue = taskInput.value;
         alert("Adding the task" + " " + taskValue);
