ee1a0942e5ff8b6ee3086f615f64f0d74d9806ae
Saarang <sahandfx@gmail.com>
2016-06-03 13:47:14 +0800

    Basic boiletplate for adding a new task

--- js/main.js
+++ js/main.js
@@ -1,9 +1,18 @@
 
 var taskInput= document.getElementById("new-task-input");
-taskInput.addEventListener("keydown", checkKeyPressed, true);
- function checkKeyPressed(e) {
+
+taskInput.addEventListener("keydown", handleKeyPress, true);
+
+function handleKeyPress(e) {
     if (e.keyCode == "13") {
-        var taskValue = taskInput.value;
-        alert("Adding the task" + " " + taskValue);
+        var taskTitle = taskInput.value;
+        createTask(taskTitle);
     }
 };
+
+function createTask(taskTitle) {
+    var newTaskElement = document.createElement('li'); // now we have '<li></li>'
+    var textElement = document.createTextNode(taskTitle); // 'go buy coffee' or whatever is inside the taskTitle
+    newTaskElement.appendChild(textElement); // now we have '<li>go buy coffee</li>'    
+    //add the newTaskElement to the ul on the page that contains the tasks
+}
