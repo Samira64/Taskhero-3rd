a992c075b016a0a64c35b0a5fb9b475b24a666e1
samira <s.soltani336@gmail.com>
2016-06-04 17:04:25 +0800

    Here is the Basic boiletplate for adding a new task

--- js/main.js
+++ js/main.js
@@ -1,18 +1,30 @@
 
-var taskInput= document.getElementById("new-task-input");
 
+
+
+function createTask(taskTitle) {
+    
+    var newTaskElement = document.createElement("li"); // now we have '<li></li>'
+    var textElement = document.createTextNode(taskTitle); // 'go buy coffee' or whatever is inside the taskTitle
+    newTaskElement.appendChild(textElement);
+    var list= document.getElementById("task-list");
+    list.appendChild(newTaskElement);
+    
+     // now we have '<li>go buy coffee</li>'    
+    //add the newTaskElement to the ul on the page that contains the tasks
+    //1- get the ul that contains the lis
+    //2- add the newTaskElement as its chidren
+    //3- commit and push. bye bye ok
+}
+
+
+var taskInput= document.getElementById("new-task-input");
 taskInput.addEventListener("keydown", handleKeyPress, true);
 
 function handleKeyPress(e) {
     if (e.keyCode == "13") {
-        var taskTitle = taskInput.value;
-        createTask(taskTitle);
+
+       var taskTitle = taskInput.value;  
+       createTask(taskTitle); 
     }
 };
-
-function createTask(taskTitle) {
-    var newTaskElement = document.createElement('li'); // now we have '<li></li>'
-    var textElement = document.createTextNode(taskTitle); // 'go buy coffee' or whatever is inside the taskTitle
-    newTaskElement.appendChild(textElement); // now we have '<li>go buy coffee</li>'    
-    //add the newTaskElement to the ul on the page that contains the tasks
-}
