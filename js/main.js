98f89398dd4fcbd164ae9e003f2a198d06a323f3
samira <s.soltani336@gmail.com>
2016-06-09 13:09:26 +0800

    Renamed the js vars and reformatted the code

--- js/main.js
+++ js/main.js
@@ -6,37 +6,32 @@ taskInput.addEventListener("keydown", handleKeyPress, true);
 function createTask(taskTitle) {
     var newTaskElement = document.createElement("li");
     var textElement = document.createTextNode(taskTitle);
-    var div1 = document.createElement("div");
-    div1.className += "check-box";
+    var inputDiv = document.createElement("div");
+    inputDiv.className += "check-box";
     var input = document.createElement("input");
     input.setAttribute("type","checkbox");
-    div1.appendChild(input);
-    var div2= document.createElement("div");
-    div2.className += "task-name";
-    div2.appendChild(textElement);
-    var div3 = document.createElement("div");
-    div3.className += "remove-circle";
+    inputDiv.appendChild(input);
+    var titleDiv= document.createElement("div");
+    titleDiv.className += "task-name";
+    titleDiv.appendChild(textElement);
+    var trashDiv = document.createElement("div");
+    trashDiv.className += "remove-circle";
     var i = document.createElement("i");
     i.className += "fa fa-trash";
-    div3.appendChild(i);
-    newTaskElement.appendChild(div1);
-    newTaskElement.appendChild(div2);  
-    newTaskElement.appendChild(div3);
-    var lists = document.getElementsByClassName("mamal");
-    lists[0].appendChild(newTaskElement);   
-
-    taskInput.value = "  ";
-}
-
+    trashDiv.appendChild(i);
+    newTaskElement.appendChild(inputDiv);
+    newTaskElement.appendChild(titleDiv);  
+    newTaskElement.appendChild(trashDiv);
+    var lists = document.getElementById("task-list");
+    lists.appendChild(newTaskElement);   
+};
 
 function handleKeyPress(e) {
     if (e.keyCode == "13") {
-     var taskTitle = taskInput.value;  
-     createTask(taskTitle);  
-        
- }
-
-
+       var taskTitle = taskInput.value;  
+       createTask(taskTitle);  
+       taskInput.value = "";
+   }
 };
 
 
